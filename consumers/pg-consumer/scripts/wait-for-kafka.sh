0dc4a5fa0ad3c371705eed77edb5aafda1c5c1c1
Adarsh-devappsys <adarsh@devappsys.com>
2025-05-09 14:24:40 +0530

    feat: update Kafka configuration for multiple brokers and enhance wait-for-kafka scripts

--- consumers/pg-consumer/scripts/wait-for-kafka.sh
+++ consumers/pg-consumer/scripts/wait-for-kafka.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
-KAFKA_HOST=${KAFKA_HOST:-kafka}
+
+# Set default Kafka host and port if not already defined as environment variables
+KAFKA_HOST=${KAFKA_HOST:-kafka-broker-1}
 KAFKA_PORT=${KAFKA_PORT:-9092}
 
 echo "Waiting for Kafka at $KAFKA_HOST:$KAFKA_PORT..."
 
-while ! nc -z "$KAFKA_HOST" "$KAFKA_PORT"; do
-  sleep 1
-  echo "Still waiting for Kafka..."
+# Use a loop to continuously check the network connection to Kafka
+until nc -z "$KAFKA_HOST" "$KAFKA_PORT"; do
+  echo "Still waiting for Kafka at $KAFKA_HOST:$KAFKA_PORT..."
+  sleep 1 # Wait for 1 second before the next attempt
 done
 
-echo "Kafka is up!"
+echo "Kafka is up and reachable at $KAFKA_HOST:$KAFKA_PORT!"
+
+# Execute the command passed as arguments to this script
+echo "Executing command: $*"
 exec "$@"
